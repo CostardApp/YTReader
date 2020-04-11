8391f1dcbb9ecfd731efc41540fc915d1894aff8
CostardApp <brian.costard@gmail.com>
2020-04-11 19:53:52 +0200

    Initial Commit

---
+++ YTReader/YTReader.h
@@ -0,0 +1,19 @@
+//
+//  YTReader.h
+//  YTReader
+//
+//  Created by Brian Costard on 11/04/2020.
+//  Copyright © 2020 Brian Costard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YTReader.
+FOUNDATION_EXPORT double YTReaderVersionNumber;
+
+//! Project version string for YTReader.
+FOUNDATION_EXPORT const unsigned char YTReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YTReader/PublicHeader.h>
+
+
